fd5e34e4e54a9c13305b69e2b990897fcd154cdf
Ksenia Karelskaya <kusksu@gmail.com>
2015-01-05 01:17:35 +0300

    readme.md

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-SVG_animations
+SVG Animations
 ==============
 
 Research project for Web Specialisation
+
+[- ML - Managing Data Exchange](http://en.wikibooks.org/wiki/XML_-_Managing_Data_Exchange/SVG)
+[Working Group](http://www.w3.org/Graphics/SVG/)
+
+
--- snap_test/js/main.js
+++ snap_test/js/main.js
@@ -42,5 +42,37 @@ function blink(){
 //Repeating animations
 setInterval(blink, 3000);
 setInterval(resize, 6000);
+setInterval(glow, 3000);
+
+// animating AE exported image
+var creature = Snap('#creature');
+var head = creature.select('#head');
+var left_eye = creature.select('#b_x5F_eye');
+var right_eye = creature.select('#s_x5F_eye');
+var lp_eye = creature.select('#p_x5F_s_x5F_eye');
+var rp_eye = creature.select('#p_x5F_b_x5F_eye');
+
+
+function glow(){
+	head.animate({fill: 'white' },1000, function() {
+		head.animate({fill: 'coral' },1000);
+	});
+	move_eye();
+}
+function move_eye(){
+	left_eye.animate({ry:0}, 100, function(){
+		left_eye.animate({ry:2}, 100);
+	});
+	right_eye.animate({ry:2}, 100, function(){
+		right_eye.animate({ry:4}, 100);
+	});
+	// console.log(mina.time());
+}
+
+
+
+
+
+
 
 
